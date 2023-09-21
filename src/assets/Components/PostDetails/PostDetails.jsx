8a8ca6a1e0a5236d73664566f2895da43a9d298c
Mmsshakil <softhandshakil@gmail.com>
2023-09-22 00:31:15 +0600

    added go back on post page by useNavigate

--- src/assets/Components/PostDetails/PostDetails.jsx
+++ src/assets/Components/PostDetails/PostDetails.jsx
@@ -1,10 +1,12 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
 
     const post = useLoaderData();
     const { id, title, body } = post;
+
+    const navigate = useNavigate();
 
     const detailStyle = {
         border: '5px solid blue',
@@ -13,11 +15,16 @@ const PostDetails = () => {
         borderRadius: '5px'
     }
 
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <div style={detailStyle}>
             <h3>Post ID: {id}</h3>
             <h5>{title}</h5>
             <p>{body}</p>
+            <button onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
