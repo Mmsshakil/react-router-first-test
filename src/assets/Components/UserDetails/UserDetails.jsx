0926529f11a6e40bd7bc39e793dca991226ec394
Mmsshakil <softhandshakil@gmail.com>
2023-09-21 21:25:58 +0600

    userDetails done with design

--- src/assets/Components/UserDetails/UserDetails.jsx
+++ src/assets/Components/UserDetails/UserDetails.jsx
@@ -2,12 +2,18 @@ import { useLoaderData } from "react-router-dom";
 
 
 const UserDetails = () => {
+    const detailStyle ={
+        border: '5px solid green',
+        padding: '10px',
+        margin: '20px',
+        borderRadius: '5px'
+    }
 
     const user = useLoaderData();
     console.log(user);
     const {name, phone, email, website} = user;
     return (
-        <div>
+        <div style={detailStyle}>
             <h2>{name}</h2>
             <p>Phone: {phone}</p>
             <p>Email: {email}</p>
