e2441a07e097dbdb87ffc2fab1851c5d9d10b98a
Mmsshakil <softhandshakil@gmail.com>
2023-09-22 00:51:45 +0600

    error message showed by useRouteError

--- src/assets/Components/ErrorPage/ErrorPage.jsx
+++ src/assets/Components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,15 @@
+import { useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
+
+    const error = useRouteError();
+    console.error(error);
+
     return (
         <div>
             <h2>Oopsss....!!!</h2>
+            <p><i>{error.data || error.statusText}  </i></p>
         </div>
     );
 };
