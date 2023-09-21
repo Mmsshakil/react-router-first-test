95a1a15235b6aa1b33fdebfc6da2fba770109c04
Mmsshakil <softhandshakil@gmail.com>
2023-09-21 18:40:13 +0600

    user style added

--- src/assets/Components/User/User.jsx
+++ src/assets/Components/User/User.jsx
@@ -4,8 +4,16 @@ const User = ({user}) => {
 
     const {id, name, email, phone} = user;
 
+    const userStyle ={
+        border : '2px solid yellow',
+        padding: '10px',
+        borderRadious: '10px'
+
+
+    }
+
     return (
-        <div>
+        <div style={userStyle}>
             <h2>{name}</h2>
             <p>Email: {email}</p>
             <p>Phone: {phone}</p>
